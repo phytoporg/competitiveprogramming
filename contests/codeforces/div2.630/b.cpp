1f8dfa4e367a0b62d3a4186a5b146439751a8803
Phil Johnson <phytoporg@gmail.com>
2020-03-31 15:19:38 -0700

    upsolving div2.630 A and B problems

--- contests/codeforces/div2.630/b.cpp
+++ contests/codeforces/div2.630/b.cpp
@@ -1,52 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int> getPrimes(int n)
+{
+    bitset<1000> isPrime(string(1000, '1'));
+    for(int p = 2; p*p <= n; ++p)
+    {
+        if (isPrime[p])
+        {
+            for (int i = p * p; i <= n; i += p)
+            {
+                isPrime[i] = false;
+            }
+        }
+    }
+
+    vector<int> primes;
+    for (int i = 2; i*i <= n; ++i)
+    {
+        if (isPrime[i]) primes.push_back(i);
+    }
+
+    return primes;
+}
+
 int main()
 {
+    vector<int> primes = getPrimes(1000);
+
     int t;
     cin >> t;
-
-    for (int k = 0; k < t; ++k)
+    for (int ti = 0; ti < t; ++ti)
     {
-        int n;
-        cin >> n;
-
+        int n; cin >> n;
         vector<int> a(n);
         for (int& ai : a) { cin >> ai; }
 
-        int m{1};
-
-        vector<int> c(n, -1);
-        for (int i = 0; i < a.size(); ++i)
+        int m{0};
+        unordered_map<int, int> cmap;
+        vector<int> c(n, 0);
+        for (int i = 0; i < n; ++i)
         {
-            if (c[i] > 0) 
-            { 
-                continue; 
-            }
-
-            int notColored{0};
-            for (int j = i; j < a.size(); ++j)
+            for (int j = 0; j < primes.size(); ++j)
             {
-                if (c[j] > 0) 
-                { 
-                    continue; 
-                }
-                else
+                if (a[i] % primes[j] == 0) 
                 {
-                    notColored++;
+                    if (!cmap.count(primes[j])) { cmap[primes[j]] = ++m; }
+                    c[i] = cmap[primes[j]];
+                    break;
                 }
-
-                if (__gcd(a[i], a[j]) > 1) { c[j] = m; }
             }
-
-            if (!notColored) { break; }
-            if (i < a.size() - 1) { ++m; };
         }
 
         cout << m << endl;
         for (int& ci : c) { cout << ci << " \n"[&ci == &c.back()]; }
     }
-
-    return 0;
 }
 
