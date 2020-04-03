76ad3cd329288de63869a408fe2b7e0a244124a6
Phil Johnson <phytoporg@gmail.com>
2020-04-02 20:25:09 -0700

    Number of products from codeforces

--- codeforces/number_of_products.cpp
+++ codeforces/number_of_products.cpp
@@ -7,13 +7,14 @@ int main()
     vector<int> a(n);
     {int i{0}; while(cin >> a[i++]);}
 
+	int sign[] = {-1, 1};
     int pos{0}; int neg{0};
     for(int i = 0; i < a.size(); ++i)
     {
         int prod = 1;
         for(int j = i; j < a.size(); ++j)
         {
-            prod *= a[j];
+            prod *= sign[a[j] > 0];
             if(prod < 0) ++neg;
             else ++pos;
         }
