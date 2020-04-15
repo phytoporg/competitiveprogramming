3d69fcb86d2bda76abafbc64a25241e8716d4149
Phil Johnson <phytoporg@gmail.com>
2020-04-15 13:51:35 -0700

    Codeforces div2 635 contest submissions.

---
+++ contests/codeforces/div2.635/a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PRINTVAR(x) (cout << #x << " is " << x << endl)
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+
+        int x, y, z;
+        x = b;    
+        y = c;
+        z = c;
+
+        cout << x << ' ' << y << ' ' << z << '\n';
+    }
+
+    return 0;
+}
+
